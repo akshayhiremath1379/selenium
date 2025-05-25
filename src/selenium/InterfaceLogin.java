3b9ebe0c90a27ac31d982bd62707a7eeefee0c6a
akshay <akshayhiremath1379@gmail.com>
2025-05-25 22:39:17 +0530

    new pagefactory

--- src/selenium/InterfaceLogin.java
+++ src/selenium/InterfaceLogin.java
@@ -2,16 +2,28 @@ package selenium;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.devtools.v135.page.Page;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class InterfaceLogin implements RulesLogin{
      WebDriver driver;
+	
+	public By buttonLogin = By.xpath("(//input[@value='Google Search'])[2]");
+	/*
+	public InterfaceLogin(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver,this);
+		
+				
+	}
+
+	@FindBy(xpath ="(//input[@value='Google Search'])[2]")
+	public WebElement buttonNew;
+	*/
+
 	@Override
 	public void green() {
-		System.out.println("hello interface");
-		// TODO Auto-generated method stub
-		
+System.out.println("Hello Interface");		
 	}
-	public By buttonLogin = By.xpath("(//input[@value='Google Search'])[2]");
 }
