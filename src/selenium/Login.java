25d8aa8bd03ae55404d8781ac07a9ce7db49e997
akshay <akshayhiremath1379@gmail.com>
2025-05-25 21:10:23 +0530

    new login

--- src/selenium/Login.java
+++ src/selenium/Login.java
@@ -1,6 +1,14 @@
 package selenium;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -9,6 +17,9 @@ import org.testng.annotations.Test;
 public class Login extends InterfaceLogin {
         
 	WebDriver driver;
+	String destDir = "C:\\Users\\IT POINT\\Documents\\selnium\\selenium\\reports";
+	String destFile = destDir + "\\screenshot.png";
+
 		// TODO Auto-generated method stub
         @BeforeTest
         public void webDriverIntialize() {
@@ -18,25 +29,33 @@ public class Login extends InterfaceLogin {
         }
         
 		@Test
-		public void test() {
+		public void test() throws IOException{
 			Login lo = new Login();
+
 			lo.green();
 			String abc = driver.findElement(lo.buttonLogin).getAttribute("value");
 
 			driver.findElement(lo.buttonLogin).click();
 			System.out.println(abc);					
-
+          File fis = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+          FileUtils.copyFile(fis, new File(destFile));
 		}
 		@Test (timeOut = 6000)
 
-		public void test2() {			
+		public void test2() {	
+			Login lo = new Login();
+
+			WebElement  abcd = driver.findElement(lo.buttonLogin);
+
+			JavascriptExecutor js = (JavascriptExecutor)driver;
+			js.executeScript("arguments[0].scrollIntoView(true);",abcd );
 			System.out.println("hello test2");		
 
 		}
 		
 		@AfterTest
 		public void closetest() {
-			driver.close();
+			driver.quit();
 
 		}
 		
